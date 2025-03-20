d4cb9987c17da2bf0775da3dbe9a0c54d5b9ae1e
jackpk <pawankumarbairwa879@gmail.com>
2025-03-20 18:11:03 +0530

    sidebar shadcn add

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,11 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import "@/styles/globals.css";
 import { ThemeProvider } from "@/providers/ThemeProvider";
 import { Toaster } from "@/components/ui/sonner";
 import Header from "@/components/Header";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import { AppSidebar } from "@/components/AppSidebar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,13 +36,15 @@ export default function RootLayout({
           enableSystem
           disableTransitionOnChange
         >
-          {" "}
-          <div className="flex flex-col h-screen">
-            <Header />
-            <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
-              {children}
-            </main>
-          </div>
+          <SidebarProvider>
+            <AppSidebar />
+            <div className="flex flex-col h-screen">
+              <Header />
+              <main className="flex flex-1 flex-col px-4 pt-10 xl:px-8">
+                {children}
+              </main>
+            </div>
+          </SidebarProvider>
           <Toaster />
         </ThemeProvider>
       </body>
